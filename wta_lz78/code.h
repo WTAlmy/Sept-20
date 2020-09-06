b0317ef5166d4160e8fba034bb067bc7dcc8dd44
William Almy <walmy@ucsc.edu>
2020-09-06 14:03:06 -0700

    Initial Commit

---
+++ wta_lz78/code.h
@@ -0,0 +1,16 @@
+//
+// Header file for code used during compression
+// See Wikipedia article on LZ78 compression
+//
+
+#ifndef __CODE_H__
+#define __CODE_H__
+
+#include <inttypes.h>
+
+#define STOP_CODE 0
+#define EMPTY_CODE 1
+#define START_CODE 2
+#define MAX_CODE UINT16_MAX
+
+#endif
